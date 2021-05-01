5317903f609b033a1921fc6cdc7078c388bf8af8
Ahmet Murat Ilgun <muratilgun34@hotmail.com>
2021-05-01 23:18:42 +0300

    Add project files.

---
+++ DapperBasicDemo/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperBasicDemo.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
